ac49572df596b81afae9640f46fb174f618e914c
Manas Hejmadi <manas2all@outlook.com>
2024-09-14 02:37:56 +0530

    FlutterWEB: TrashTag & Crossplatform QR Scanner Implementation

--- Application/trashtag/lib/main.dart
+++ Application/trashtag/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trashtag/extensions/extensions.dart';
 import 'package:trashtag/home.dart';
 
 void main() {
@@ -13,7 +14,27 @@ class TrashTagApplication extends StatelessWidget {
     return MaterialApp(
       title: "TrashTag",
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: WebLimiter(),
     );
+  }
+}
+
+class WebLimiter extends StatelessWidget {
+  const WebLimiter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    if (width > 600) {
+      return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+                  'Desktop Web Version Unsupported! Please use TrashTag on a Mobile Device')
+              .color(Colors.white),
+        ),
+      );
+    }
+    return const Home();
   }
 }
